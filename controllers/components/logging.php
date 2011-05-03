7360f0759e617481847df0b6ee4e78ee254ec9ac
Jose Diaz-Gonzalez <myaccounts@savant.be>
2011-05-02 18:33:34 -0700

    Auto-import user data if Authsome component is in use

--- controllers/components/logging.php
+++ controllers/components/logging.php
@@ -10,7 +10,13 @@ class LoggingComponent extends Object {
 		if (!count($controller->uses) || get_parent_class($controller->{$controller->modelClass}) == 'Object') return;
 		if (!$controller->{$controller->modelClass}->Behaviors->attached('Logable')) return;
 
-		if (!Authsome::get('guest')) $controller->{$controller->modelClass}->setUserData(Authsome::get());
+		// Auto-import user data if Authsome component is in use
+		if (class_exists('Authsome')) {
+			if (!Authsome::get('guest')) {
+				$controller->{$controller->modelClass}->setUserData(Authsome::get());
+			}
+		}
+
 		$controller->{$controller->modelClass}->setRequestParameters($controller->params);
 		$controller->{$controller->modelClass}->setUserBrowser($_SERVER['HTTP_USER_AGENT']);
 		$controller->{$controller->modelClass}->setUserIp($_SERVER['REMOTE_ADDR']);
